cd6f056fe21695a97e763a4829c2d215f912cbd2
amada10 <amada.rktvo@gmail.com>
2022-01-30 01:29:00 +0300

    ajout menu

---
+++ src/components/navbar/IndexNavBar.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+
+import {
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+} from "react-pro-sidebar";
+
+import { FaList } from "react-icons/fa";
+import {BsCreditCard} from "react-icons/bs";
+import { MdPayment, MdOutlineContactMail, MdOutlineSettings } from "react-icons/md";
+import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import { RiPencilLine, RiAccountCircleLine } from "react-icons/ri";
+
+
+import "react-pro-sidebar/dist/css/styles.css";
+import "../../assets/css/Index.css";
+
+
+const IndexNavBar = () => {
+  
+    const [menuCollapse, setMenuCollapse] = useState(false)
+
+  const menuIconClick = () => {
+    menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
+  };
+
+  return (
+    <>
+      <div id="header">
+        <ProSidebar collapsed={menuCollapse}>
+          <SidebarHeader>
+          <div className="logotext">
+              <p>{menuCollapse ? "Bank" : "Klamma banking"}</p>
+            </div>
+            <div className="closemenu" onClick={menuIconClick}>
+              {menuCollapse ? (
+                <FiArrowRightCircle/>
+              ) : (
+                <FiArrowLeftCircle/>
+              )}
+            </div>
+          </SidebarHeader>
+          <SidebarContent>
+            <Menu iconShape="square">
+              <MenuItem active={true} icon={<FiHome />}>
+                Overview
+              </MenuItem>
+              <MenuItem icon={<BsCreditCard />}>Cards</MenuItem>
+              <MenuItem icon={<MdPayment />}>Payments</MenuItem>
+              <MenuItem icon={<RiPencilLine />}>Repots</MenuItem>
+              <MenuItem icon={<MdOutlineContactMail />}>Contacts</MenuItem>
+            </Menu>
+          </SidebarContent>
+          <SidebarFooter>
+            <Menu iconShape="square">
+              <MenuItem icon={<MdOutlineSettings />}>Settings</MenuItem>
+              <MenuItem icon={<RiAccountCircleLine />}>Account</MenuItem>
+            </Menu>
+          </SidebarFooter>
+        </ProSidebar>
+      </div>
+    </>
+  );
+};
+
+export default IndexNavBar;
---
+++ src/view/Index.js
@@ -0,0 +1,13 @@
+import '../assets/css/Index.css';
+import IndexNavBar from '../components/navbar/IndexNavBar';
+
+
+function Index() {
+  return (
+    <div className="App">
+      <IndexNavBar />
+    </div>
+  );
+}
+
+export default Index;
